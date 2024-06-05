353fe33897be7853f6846557ef9c3faba003eb58
Jose1277 <josee.oliveira022@gmail.com>
2024-06-05 01:27:22 -0300

    primeiro scraping

--- projeto/server/routing.js
+++ projeto/server/routing.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const exampleRouter = require('./api/example.route');
 const homeRouter = require('./api/home.route');
+const scrapingRouter = require('./api/scraping.route');
 
 function routing(app) {
     app.use('/' , homeRouter);
     app.use('/api', exampleRouter);
+    app.use('/scraping', scrapingRouter);
 }
 
 module.exports = routing;
