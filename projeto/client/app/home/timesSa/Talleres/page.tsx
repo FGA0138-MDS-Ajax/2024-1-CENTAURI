c255ce2a8b8d2b617cb2142fff4495a942bb2849
fabinsz <fabio2003gsaa@gmail.com>
2024-06-26 16:18:56 -0300

    att sidebar

---
+++ projeto/client/app/home/timesSa/Talleres/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Bebas_Neue } from 'next/font/google';
+import { cn } from '@/lib/utils';
+
+
+const font = Bebas_Neue({
+  subsets: ['latin'],
+  weight: ['400'],
+});
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center flex-grow">
+      <img src="https://logodetimes.com/times/talleres/logo-talleres-4096.png" alt="Talleres" width={100} height={100} />
+        <div className="space-y-6 text-center">
+         
+         <h1
+            className={cn(
+              'text-6xl font-semibold text-white drop-shadow-md',
+              font.className
+            )}
+          >
+            Talleres
+          </h1>
+          <p className="text-lg text-black">
+            Chega de ficar se perguntando onde o jogo do seu time favorito será transmitido
+          </p>
+        </div>
+    </main>
+
+  );
+}
