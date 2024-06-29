11389e67e8bfe89ca8a8b45f942419e3082e460e
Jose1277 <josee.oliveira022@gmail.com>
2024-06-29 15:55:24 -0300

    rota post register

--- projeto/server/src/routes/auth.route.ts
+++ projeto/server/src/routes/auth.route.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import {createUsuarioSchema} from '../schemas/auth.schema';
+import AuthRepository from '../repositories/auth.repository';
 
 const router = Router();
 
@@ -8,5 +10,16 @@ router.get('/v1/favorito', async (req, res) => {
     }
     res.send(favoritoJson);
 });
+
+router.post('/register', async(req, res) => {
+    const user = createUsuarioSchema.parse(req.body);
+    try{
+        const result = await AuthRepository.registerUser(user);
+        res.send(result);
+    }
+    catch(err) {
+        res.status(400).send(err);
+    }
+})
 
 export default router;
